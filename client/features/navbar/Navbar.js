9641affd69574cfae7b7e4965e2a89367e01f5c2
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-02-28 17:29:27 -0500

    Update Navbar.js

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
 					<div>
 						<Link to="/home">Home</Link>
 						<Link to="/men">Men</Link>
-						<Link to="/women">Men</Link>
+						<Link to="/women">Women</Link>
 						{/* The navbar will show these links before you log in */}
 						<Link to="/login">Login</Link>
 						<Link to="/signup">Sign Up</Link>
