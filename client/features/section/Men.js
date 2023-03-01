d7481cc0938cc930de2d906099b7b46fb6ebb034
Syed Raza <syed.raza011@gmail.com>
2023-03-01 18:16:52 -0500

    men section

--- client/features/section/Men.js
+++ client/features/section/Men.js
@@ -3,36 +3,50 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchAllProducts } from "../../app/slices/productsSlice";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import SingleProduct from "../SingleProduct/SingleProduct";
 
-function Men(props) {
-    const dispatch = useDispatch();
+/**
+ * COMPONENT
+ */
+const Men = (props) => {
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(fetchAllProducts());
-    }, []);
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, []);
 
-    const username = useSelector((state) => state.auth.me.username);
-    const products = useSelector((state) => {
-      return state.products.products.flat();
+  const username = useSelector((state) => state.auth.me.username);
+  const products = useSelector((state) => {
+    state.products.products.filter((product) => {
+      product.categoryId === 2;
     });
+    console.log("filtered products", state.products.products);
+    return state.products.products.flat();
+  });
 
-    return (
-      <div
-        onClick={() => {
-          console.log(products);
-        }}
-      >
-        <h3>Men</h3>
-        {products.map((product) => {
-          return (
-            <Link key={uuidv4()} to={`/product/${product.id}`}>
-              {product.name}
-            </Link>
-          );
-        })}
-      </div>
-    );
-  };
+  return (
+    <div
+      onClick={() => {
+        // console.log(products);
+      }}
+    >
+      <h3>Welcome, {username}</h3>
+      {products.map((product) => {
+        return (
+          <>
+            <div class="card">
+              <p>{product.name}</p>
+              <p>{product.imageUrl}</p>
+              <p>{product.price}</p>
+              <p>{product.description}</p>
+              <button key={uuidv4()} to={`/products/${product.id}`}>
+                product Details
+              </button>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Men;
--- server/db/db.js
+++ server/db/db.js
@@ -23,7 +23,7 @@ if (process.env.DATABASE_URL) {
 
 // "postgres://localhost:5432/backupGrace"
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://grace_db_ulq1_user:qZv5AJSpF0WqK75RP0jZoQphoGuITjc7@dpg-cfv5p7l3t39doaohcdlg-a/grace_db_ulq1?ssl:true`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/backupgrace`,
   config
 );
 module.exports = db;
