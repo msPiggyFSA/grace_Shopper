fd0cf84c37dff5ea6759149bb3af0d580eea4fa9
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-03-02 15:38:58 -0500

    added delete button to single product, still need to put in conditional

--- client/features/SingleProduct/SingleProduct.js
+++ client/features/SingleProduct/SingleProduct.js
@@ -3,6 +3,7 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import { fetchSingleProduct } from "../../app/slices/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Add2Cart from "../cart/Add2Cart";
+import DeleteProduct from "../admin/DeleteProduct"
 import "./css/SingleProduct.css";
 
 const SingleProduct = (props) => {
@@ -10,6 +11,8 @@ const SingleProduct = (props) => {
   const navigate = useNavigate();
   const params = useParams();
   const dispatch = useDispatch();
+  const admin = useSelector((state)=>state.auth.me.admin)
+
   //   useEffect(() => {
   //     if (params === "number") {
   //       dispatch(fetchSingleProduct(params));
@@ -24,7 +27,7 @@ const SingleProduct = (props) => {
       <div onClick={() => navigate(`/product/${product.id}`)}>
         {product.name}
       </div>
-
+      <DeleteProduct props={product}/>
       <Add2Cart props={product} />
     </div>
   );
