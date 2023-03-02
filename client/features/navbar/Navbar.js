9d051e28507a9307b69027d410a3af0edc85b149
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-03-02 18:39:35 -0500

    if user is admin, navbar display add new product link

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -8,37 +8,50 @@ const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const isAdmin = useSelector((state)=>state.auth.me.admin)
   const logoutAndRedirectHome = () => {
     dispatch(logout());
     navigate("/login");
   };
 
   return (
-    <div>
-      <h1>FS-App-Template</h1>
-      <nav>
-        <Link to="/home">Home</Link>
-        <Link to="/category/1">Men</Link>
-        <Link to="/category/2">Women</Link>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
+		<div>
+			<h1>FS-App-Template</h1>
+			<nav>
+				<Link to="/home">Home</Link>
+				<Link to="/category/1">Men</Link>
+				<Link to="/category/2">Women</Link>
+				{isLoggedIn ? (
+					(
+						<div>
+							{/* The navbar will show these links after you log in */}
+							<button type="button" onClick={logoutAndRedirectHome}>
+								Logout
+							</button>
+						</div>
+					) && (isAdmin === true )? (
+            <>
+						<Link to='/addprod'>add new product</Link>
             <button type="button" onClick={logoutAndRedirectHome}>
-              Logout
-            </button>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login/Signup</Link>
-            {/* <Link to="/signup">Sign Up</Link> */}
-          </div>
-        )}
-        <Cart />
-      </nav>
-      <hr />
-    </div>
-  );
+								Logout
+							</button>
+              </>
+					) : (
+						<div>
+							{/* The navbar will show these links before you log in */}
+							<Link to="/login">Login/Signup</Link>
+						</div>
+					)
+				) : (
+					<>
+						<Link to="/login">Login/Signup</Link>
+					</>
+				)}
+				<Cart />
+			</nav>
+			<hr />
+		</div>
+	);
 };
 
 export default Navbar;
