dc3ffb7317c875dca43c8617b1ccf43a4dd2fec7
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-03-02 15:43:40 -0600

    fixed login/signup routes

--- client/features/SingleProduct/SingleProduct.js
+++ client/features/SingleProduct/SingleProduct.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { fetchSingleProduct, deleteProduct, fetchAllProducts } from "../../app/slices/productsSlice";
+import {
+  fetchSingleProduct,
+  deleteProduct,
+  fetchAllProducts,
+} from "../../app/slices/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Add2Cart from "../cart/Add2Cart";
 
@@ -8,18 +12,16 @@ import "./css/SingleProduct.css";
 
 const SingleProduct = (props) => {
   const product = props.props;
-  console.log(product);
+  // console.log(product);
   const navigate = useNavigate();
   const params = useParams();
   const dispatch = useDispatch();
   const admin = useSelector((state) => state.auth.me.admin);
-  console.log(admin)
+  console.log(admin);
   const handleDelete = (id) => {
-    dispatch(deleteProduct(id))
+    dispatch(deleteProduct(id));
     dispatch(fetchAllProducts());
-  }
-
-
+  };
 
   //   useEffect(() => {
   //     if (params === "number") {
@@ -36,11 +38,14 @@ const SingleProduct = (props) => {
         {product.name}
       </div>
 
-      
-      {admin===true?
-      <><Add2Cart props={product} /><button onClick={() => handleDelete(product.id)}>X</button></>:
-      <Add2Cart props={product} />}
-      
+      {admin === true ? (
+        <>
+          <Add2Cart props={product} />
+          <button onClick={() => handleDelete(product.id)}>X</button>
+        </>
+      ) : (
+        <Add2Cart props={product} />
+      )}
     </div>
   );
 };
--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { authenticate } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { authenticate } from "../../app/store";
+import { Link, useNavigate } from "react-router-dom";
 
 /**
   The AuthForm component can be used for Login or Sign Up.
@@ -11,6 +12,7 @@ import { authenticate } from '../../app/store';
 const AuthForm = ({ name, displayName }) => {
   const { error } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -18,11 +20,22 @@ const AuthForm = ({ name, displayName }) => {
     const username = evt.target.username.value;
     const password = evt.target.password.value;
     dispatch(authenticate({ username, password, method: formName }));
+    if (!error) {
+      // navigate("/home");
+    } else {
+      // navigate("/home");
+    }
+    console.log(error);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
+      <form
+        onSubmit={() => {
+          handleSubmit();
+        }}
+        name={name}
+      >
         <div>
           <label htmlFor="username">
             <small>Username</small>
@@ -40,6 +53,9 @@ const AuthForm = ({ name, displayName }) => {
         </div>
         {error && <div> {error} </div>}
       </form>
+      {name === "login" && (
+        <Link to="/signup">Click here to create an account!</Link>
+      )}
     </div>
   );
 };
