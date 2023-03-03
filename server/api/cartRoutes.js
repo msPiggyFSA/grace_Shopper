0cf5dc9fe273638e29f6057e97ef46cf020d2b37
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-03-03 17:51:19 -0500

    now api/carts show user

--- server/api/cartRoutes.js
+++ server/api/cartRoutes.js
@@ -1,16 +1,19 @@
-const Cart = require('../db/models/Cart')
-const CartProduct = require('../db/models/CartProduct')
+const {
+	models: { CartProduct, Cart, User, Product },
+} = require("../db");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get("/", async(req, res, next)=>{
-    try {
-        const cart = await Cart.findAll({
-            include: CartProduct
-        })
-        res.send(cart)
-    } catch (error) {
-        console.log(error.message)
-    }
-})
-module.exports = router
+router.get("/", async (req, res, next) => {
+	try {
+		const cart = await Cart.findAll({
+			include: [
+                {model: CartProduct}, 
+                {model: User}],
+		});
+		res.send(cart);
+	} catch (error) {
+		console.log(error.message);
+	}
+});
+module.exports = router;
