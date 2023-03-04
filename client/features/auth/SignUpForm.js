c781b4dd243d2ccab1bdd8745b3a13706b6bd806
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-03-03 22:36:12 -0500

    ready to passdown form to slice for user sign up

--- client/features/auth/SignUpForm.js
+++ client/features/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { authenticate } from "../../app/store";
 import { Link, useNavigate } from "react-router-dom";
@@ -10,6 +10,15 @@ import { Link, useNavigate } from "react-router-dom";
 **/
 
 const SignUpForm = ({ name, displayName }) => {
+  const [form, setForm] = useState({
+    username: "", 
+    password: '',
+    email: '',
+    fName:'',
+    lName:''
+  })
+  console.log(form)
+
   const { error } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -25,9 +34,16 @@ const SignUpForm = ({ name, displayName }) => {
     } else {
       // navigate("/home");
     }
+
+  
     console.log(error);
   };
-
+const changeValue = (prop)=>(event)=>{
+    setForm({
+        ...form,
+        [prop]:event.target.value
+    })
+  }
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
@@ -36,30 +52,30 @@ const SignUpForm = ({ name, displayName }) => {
           <label htmlFor="fName">
             <small>First Name</small>
           </label>
-          <input name="fName" type="fName" />
+          <input name="fName" type="fName" value={form.fName} onChange={changeValue('fName')}/>
         </div>
         <div>
           <label htmlFor="lName">
             <small>Last Name</small>
           </label>
-          <input name="lName" type="lName" />
+          <input name="lName" type="lName" value={form.lName} onChange={changeValue('lName')}/>
         </div>
           <label htmlFor="username">
             <small>Username</small>
           </label>
-          <input name="username" type="text" />
+          <input name="username" type="text" value={form.username} onChange={changeValue('username')}/>
         </div>
         <div>
           <label htmlFor="password">
             <small>Password</small>
           </label>
-          <input name="password" type="password" />
+          <input name="password" type="password" value={form.password} onChange={changeValue('password')} />
         </div>
         <div>
           <label htmlFor="email">
             <small>Email</small>
           </label>
-          <input name="email" type="email" />
+          <input name="email" type="text" value={form.email} onchange={changeValue('email')}/>
         </div>
         <div>
           <button type="submit">{displayName}</button>
