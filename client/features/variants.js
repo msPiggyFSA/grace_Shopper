7d0bf5f5355c4c8863ed72744af21712baa9005c
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-03-05 17:30:29 -0600

    fixed single prod view

--- client/features/variants.js
+++ client/features/variants.js
@@ -1,9 +1,9 @@
 export const container = {
-  initial: { x: -2000 },
+  initial: { x: -3000 },
   visible: { x: 0, transition: { duration: 1.5 } },
   exit: { x: -2000, transition: { duration: 0.5 } },
   prev: { x: -2000, transition: { duration: 0.5 } },
-  next: { y: 2000, transition: { duration: 0.5 } },
+  next: { x: 3000, transition: { duration: 0.5 } },
 };
 // initial={{ x: -300 }}
 //       animate={{ x: 0 }}
