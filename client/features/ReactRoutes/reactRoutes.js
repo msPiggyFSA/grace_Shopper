a7dd3d2e3d517393f0a2716c84ec2c5b252d5697
jon <jkwei.86@gmail.com>
2023-03-07 11:20:11 -0500

    start up bug fixed for log in

--- client/features/ReactRoutes/reactRoutes.js
+++ client/features/ReactRoutes/reactRoutes.js
@@ -8,6 +8,9 @@ import SingleProductView from "../SingleProduct/SingleProductView";
 import Category from "../section/Category";
 import CartView from "../cart/CartView";
 import AddProduct from "../admin/AddProduct";
+import AuthForm from "../auth/AuthForm";
+import SignUpForm from "../auth/SignUpForm";
+// import CartProducts from "../cart/CartProduct";
 import EditProduct from "../admin/EditProduct";
 import UserProfile from "../user/UserProfile";
 import EditUserProfilePage from "../user/EditUserProfilePage";
@@ -15,45 +18,47 @@ import { AnimatePresence } from "framer-motion";
 import { useSelector } from "react-redux";
 import AdminSingleUser from "../admin/AdminSingleUser";
 import AdminAllUserView from "../admin/AdminAllUserView";
-// import UserOrderHistory from "../user/UserOrderHistory";
-// import	Combined from "../combined/Combined"
+import Combined from "../auth/Combined.js";
 
 const ReactRoute = () => {
   const isAdmin = useSelector((state) => state.auth.me.admin) === true;
   const location = useLocation();
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   return (
-    <AnimatePresence node="wait">
-      <Routes location={location} key={location.key}>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/refund" element={<Refund />} />
-        <Route path="/product/:id" element={<SingleProductView />} />
-        <Route path="/category/:id" element={<Category />} />
-        <Route path="/cart" element={<CartView />} />
-        {/* <Route path="/cartProducts" element={<CartProducts />} /> */}
-        {/* <Route path="/login" element={<Combined />} />
-				<Route path="/signup" element={<Combined />} /> */}
-        <Route path="/users/:id" element={<UserProfile />} />
-        {isLoggedIn && isAdmin ? (
+		<AnimatePresence node="wait">
+			<Routes location={location} key={location.key}>
+				<Route path="/" element={<Home />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/refund" element={<Refund />} />
+				<Route path="/product/:id" element={<SingleProductView />} />
+				<Route path="/category/:id" element={<Category />} />
+				<Route path="/cart" element={<CartView />} />
+				{/* <Route path="/cartProducts" element={<CartProducts />} /> */}
+				<Route path="/login" element={<Combined />} />
+				<Route path="/signup" element={<Combined />} />
+				<Route path="/users/:id" element={<UserProfile />} />
+				{isLoggedIn && isAdmin ? (
+					<>
+						<Route path="/addprod" element={<AddProduct />} />
+						<Route path="/product/edit/:id" element={<EditProduct />} />
+						<Route path="/users/:id" element={<UserProfile />} />
+						<Route path="/admin/viewAllUsers" element={<AdminAllUserView />} />
+						<Route path="/admin/users/:id" element={<AdminSingleUser />} />
+						<Route path="/admin/edit/users/:id" element={<EditUserProfilePage />} />
+					</>
+				) : isLoggedIn ? (
           <>
-            <Route path="/addprod" element={<AddProduct />} />
-            <Route path="/product/edit/:id" element={<EditProduct />} />
             <Route path="/users/:id" element={<UserProfile />} />
-            <Route path="/viewAllUsers" element={<AdminAllUserView />} />
-            <Route path="/admin/users/:id" element={<AdminSingleUser />} />
             <Route path="/edit/users/:id" element={<EditUserProfilePage />} />
           </>
-        ) : isLoggedIn ? (
-          <Route path="/users/:id" element={<UserProfile />} />
-        ) : (
-          <></>
-        )}
-      </Routes>
-    </AnimatePresence>
-  );
+				) : (
+					<></>
+				)}
+			</Routes>
+		</AnimatePresence>
+	);
 };
 
 export default ReactRoute;
