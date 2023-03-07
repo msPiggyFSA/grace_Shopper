b95e003abab0597f18b7f5f44afad9938a63c182
jon <jkwei.86@gmail.com>
2023-03-07 14:36:09 -0500

    4th try :(

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-import cors from "cors";
+const cors = require("cors");
 
 
 app.use(cors());
