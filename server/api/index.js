c62da8254e5b377a60630a02677ebe100c75609c
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-03-07 12:51:01 -0500

    created order confirmation pages for after stripe

--- server/api/index.js
+++ server/api/index.js
@@ -19,23 +19,23 @@ router.post('/create-checkout-session', async (req, res)=>{
  
   try { console.log(req.body, 'this is request.body')
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
-      line_items: req.body.items.map(item=>{
-        return{
-          price_data:{
-            currency: 'usd',
-            product_data:{
-              name: item.name
-            }, 
-            unit_amount: item.price * 100
-          },
-          quantity: 1
-        }
-      }),
-      success_url: `${process.env.SERVER_URL}/success.html`,
-      cancel_url: `${process.env.SERVER_URL}/cancel.html`,
-    })
+			payment_method_types: ["card"],
+			mode: "payment",
+			line_items: req.body.items.map((item) => {
+				return {
+					price_data: {
+						currency: "usd",
+						product_data: {
+							name: item.name,
+						},
+						unit_amount: item.price * 100,
+					},
+					quantity: 1,
+				};
+			}),
+			success_url: `${process.env.SERVER_URL}/order/success`,
+			cancel_url: `${process.env.SERVER_URL}/order/unsuccess`,
+		});
     res.json({url: session.url})
   } catch (error) {
     res.status(500).json({error: error.message})
